da4a17ab26c001030925df0aaaeae1d1c4eed01a
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-03 13:31:27 +1200

    Modify layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,43 +1,51 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import * as React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import { UiProvider } from "@cabezonidas/shop-ui";
+import Shell from "./Shell";
 
 type Props = {
-  title?: string
-}
+  title?: string;
+};
 
-const Layout: React.FunctionComponent<Props> = ({
-  children,
-  title = 'This is the default title',
-}) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+const Layout: React.FunctionComponent<Props> = (props) => (
+  <UiProvider suspense={false}>
+    <LayoutInternal {...props} />
+  </UiProvider>
+);
 
-export default Layout
+const LayoutInternal: React.FunctionComponent<Props> = (props) => {
+  const { title, children } = props;
+  return (
+    <Shell display="grid" gridTemplateRows="auto 1fr auto">
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>{" "}
+          |{" "}
+          <Link href="/about">
+            <a>About</a>
+          </Link>{" "}
+          |{" "}
+          <Link href="/users">
+            <a>Users List</a>
+          </Link>{" "}
+          | <a href="/api/users">Users API</a>
+        </nav>
+      </header>
+      {children}
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </Shell>
+  );
+};
+
+export default Layout;
