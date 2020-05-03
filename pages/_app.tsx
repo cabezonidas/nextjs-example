32955c3ba2c2ad0af83244f3620a6e188b776711
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-03 16:51:06 +1200

    Proof of concept to keep state between routes

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from "next/app";
+import { UiProvider } from "@cabezonidas/shop-ui";
+import { createContext, useContext, useState } from "react";
+
+const StateContext = createContext<{
+  text?: string;
+  setText: (t?: string) => void;
+}>(undefined as any);
+export const useAppContext = () => useContext(StateContext);
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [text, setText] = useState<string>();
+  return (
+    <UiProvider suspense={false}>
+      <StateContext.Provider value={{ text, setText }}>
+        <Component {...pageProps} />
+      </StateContext.Provider>
+    </UiProvider>
+  );
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import { useAppContext } from "./_app";
+import { Form, Label, Input, Box } from "@cabezonidas/shop-ui";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const { text, setText } = useAppContext();
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <Box p="2" maxWidth="24rem">
+        <Box fontSize="4" mb="3">
+          <h1>Hello Visitor 👋</h1>
+        </Box>
+        <Form onSubmit={(e) => e.preventDefault()}>
+          <Label htmlFor="text">App variable</Label>
+          <Input
+            id="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <Box>Proof that I can persist state!</Box>
+        </Form>
+      </Box>
+    </Layout>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
