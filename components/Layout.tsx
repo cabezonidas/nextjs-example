1f1fe75f0f17994ab9b4171f85022afdc5fbce0d
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-03 16:50:38 +1200

    Brand layout page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,59 @@
 import * as React from "react";
 import Link from "next/link";
 import Head from "next/head";
-import { UiProvider } from "@cabezonidas/shop-ui";
 import Shell from "./Shell";
+import { useTranslation, Box, styled } from "@cabezonidas/shop-ui";
 
 type Props = {
   title?: string;
 };
 
-const Layout: React.FunctionComponent<Props> = (props) => (
-  <UiProvider suspense={false}>
-    <LayoutInternal {...props} />
-  </UiProvider>
-);
+const enUsRoutes = {
+  routes: {
+    home: "Home",
+    about: "About",
+    usersList: "Users list",
+    usersApi: "Users API",
+  },
+  footer: "I'm here to stay!",
+};
+const esArRoutes = {
+  routes: {
+    home: "Inicio",
+    about: "Acerca",
+    usersList: "Lista de usuarios",
+    usersApi: "API de usuarios",
+  },
+  footer: "Estoy para quedarme!",
+};
 
-const LayoutInternal: React.FunctionComponent<Props> = (props) => {
+const Nav = styled(Box)(
+  ({ theme }) => `
+padding: ${theme.space[4]} ${theme.space[4]};
+background: ${theme.colors.neutral.dark};
+color: ${theme.colors.neutral.lightest};
+`
+).withComponent("nav");
+
+const Footer = Nav.withComponent(Nav);
+
+const Layout: React.FunctionComponent<Props> = (props) => {
   const { title, children } = props;
+  const { t, i18n } = useTranslation();
+  i18n.addResourceBundle(
+    "en-US",
+    "translation",
+    { layout: enUsRoutes },
+    true,
+    true
+  );
+  i18n.addResourceBundle(
+    "es-AR",
+    "translation",
+    { layout: esArRoutes },
+    true,
+    true
+  );
   return (
     <Shell display="grid" gridTemplateRows="auto 1fr auto">
       <Head>
@@ -24,26 +62,23 @@ const LayoutInternal: React.FunctionComponent<Props> = (props) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <header>
-        <nav>
-          <Link href="/">
-            <a>Home</a>
-          </Link>{" "}
-          |{" "}
-          <Link href="/about">
-            <a>About</a>
-          </Link>{" "}
-          |{" "}
-          <Link href="/users">
-            <a>Users List</a>
-          </Link>{" "}
-          | <a href="/api/users">Users API</a>
-        </nav>
+        <Nav display="flex" justifyContent="space-between">
+          <Box display="grid" gridGap="2" gridTemplateColumns="repeat(4, auto)">
+            <Link href="/">
+              <a>{t("layout.routes.home")}</a>
+            </Link>
+            <Link href="/about">
+              <a>{t("layout.routes.about")}</a>
+            </Link>
+            <Link href="/users">
+              <a>{t("layout.routes.usersList")}</a>
+            </Link>
+          </Box>
+          <a href="/api/users">{t("layout.routes.usersApi")}</a>
+        </Nav>
       </header>
-      {children}
-      <footer>
-        <hr />
-        <span>I'm here to stay (Footer)</span>
-      </footer>
+      <Box>{children}</Box>
+      <Footer>{t("layout.footer")}</Footer>
     </Shell>
   );
 };
