8b27269cf3e731e98f6bb82ab6c4bd38bff26c59
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-06-30 20:06:13 +1200

    Improved pinned example (beta)

--- pages/pinned/[id].tsx
+++ pages/pinned/[id].tsx
@@ -1,8 +1,67 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { initializeApollo } from "../../lib/apolloClient";
+import {
+  PinnedPostsDocument,
+  PinnedPostsQuery,
+  PostFragment,
+} from "../../graphql-queries";
+import { GetStaticPaths, GetStaticProps } from "next";
 
-export default class StaticPropsDetail extends React.Component {
+type Props = {
+  item?: PostFragment;
+  errors?: string;
+};
+
+export default class StaticPropsDetail extends React.Component<Props> {
   render() {
-    return <Layout title={`Pinned Posts`}>Test</Layout>;
+    const { item, errors } = this.props;
+    if (errors) {
+      return (
+        <Layout title={`Error | Next.js + TypeScript Example`}>
+          <p>
+            <span style={{ color: "red" }}>Error:</span> {errors}
+          </p>
+        </Layout>
+      );
+    }
+    return <Layout title={`Pinned Posts`}>{item?.title}</Layout>;
   }
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const apollo = initializeApollo();
+  const { data } = await apollo.query<PinnedPostsQuery>({
+    query: PinnedPostsDocument,
+  });
+
+  const paths = data.getPinnedPublicPosts.map((p) => ({
+    params: { id: p._id },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+};
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const apollo = initializeApollo();
+  const {
+    data: { getPinnedPublicPosts: pinned },
+  } = await apollo.query<PinnedPostsQuery>({
+    query: PinnedPostsDocument,
+  });
+
+  try {
+    const id = params?.id;
+    const item = pinned.find((p) => p._id === id);
+    // By returning { props: item }, the StaticPropsDetail component
+    // will receive `item` as a prop at build time
+    return { props: { item } };
+  } catch (err) {
+    return { props: { errors: err.message } };
+  }
+};
--- pages/pinned/index.tsx
+++ pages/pinned/index.tsx
@@ -1,9 +1,42 @@
 import Layout from "../../components/Layout";
+import { GetStaticProps } from "next";
+import { initializeApollo } from "../../lib/apolloClient";
+import {
+  PinnedPostsQuery,
+  PinnedPostsDocument,
+  PostFragment,
+} from "../../graphql-queries";
+import { Box } from "@cabezonidas/shop-ui";
+import Link from "next/link";
 
-const WithStaticProps = () => (
+type Props = {
+  items?: PostFragment[];
+};
+
+const WithStaticProps = ({ items }: Props) => (
   <Layout title="Pinned">
     <h1>Pinned</h1>
+    {items?.map((i, index) => (
+      <Box key={i._id}>
+        <Link href="/pinned/[id]" as={`/pinned/${i._id}`}>
+          <a>
+            Post # {index}: {i.title}
+          </a>
+        </Link>
+      </Box>
+    ))}
   </Layout>
 );
 
 export default WithStaticProps;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const apollo = initializeApollo();
+  const {
+    data: { getPinnedPublicPosts: pinned },
+  } = await apollo.query<PinnedPostsQuery>({
+    query: PinnedPostsDocument,
+  });
+
+  return { props: { items: pinned } };
+};
