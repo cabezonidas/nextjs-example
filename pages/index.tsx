d547e3cab95ccc2c1c9d7b7d47387de476f274c9
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-07-01 18:22:15 +1200

    Get all entries inside index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,92 @@
 import Layout from "../components/Layout";
-import Head from "next/head";
-import { useTranslation, H1 } from "@cabezonidas/shop-ui";
+import { useTranslation, H1, Box } from "@cabezonidas/shop-ui";
+import { initializeApollo } from "../lib/apolloClient";
+import {
+  GetLatestPublicPostsDocument,
+  GetLatestPublicPostsQuery,
+  useGetLatestPublicPostsLazyQuery,
+} from "../graphql-queries";
+import { InferGetStaticPropsType } from "next";
+import { PostView } from "../components/PostView";
+import { usePostTranslation } from "../utils/helpers";
+import { Fragment, useState, useEffect, useMemo } from "react";
+import { PostPreview } from "../components/PostPreview";
+import { companyName } from "../utils/config";
 
 const enUs = {
-  title: "Welcome to Latam Trading Club",
+  olderPosts: "Previous entries",
 };
 
 const esAr = {
-  title: "Bienvenidos a Latam Trading Club",
+  olderPosts: "Últimas entradas",
 };
 
-const IndexPage = () => {
+const IndexPage = ({
+  items,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { t, i18n } = useTranslation();
+  const { getTranslatedPost } = usePostTranslation();
   i18n.addResourceBundle("en-US", "translation", { index: enUs }, true, true);
   i18n.addResourceBundle("es-AR", "translation", { index: esAr }, true, true);
 
+  const [latest, ...rest] = items.posts;
+  const oldPostsTotal = useMemo(() => Math.max(items.total - 1, 0), []);
+  const translatedLatest = getTranslatedPost(latest);
+
+  const [olderPosts, setOlderPosts] = useState(rest);
+
+  const [fetchMore, { loading, data }] = useGetLatestPublicPostsLazyQuery();
+  useEffect(() => {
+    if (data?.getLatestPublicPosts) {
+      setOlderPosts((op) => [...op, ...data.getLatestPublicPosts.posts]);
+    }
+  }, [data]);
+
+  const onScroll = () => {
+    if (!loading && olderPosts.length < oldPostsTotal) {
+      fetchMore({ variables: { skip: olderPosts.length + 1, take: 1 } });
+    }
+  };
+
   return (
-    <Layout title="Latam Trading Club">
-      <Head>
-        <title>{"Latam Trading Club"}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <H1>{t("index.title")}</H1>
+    <Layout title={companyName} onMainScrollBottom={onScroll}>
+      <Box>
+        {translatedLatest && <PostView data={translatedLatest} />}
+        {olderPosts.length > 0 && (
+          <>
+            <H1 mt="6" mb="2">
+              {t("index.olderPosts")}
+            </H1>
+            <Box display="grid" gridGap="6" mb="6">
+              {olderPosts.map((old, index) => {
+                const translatedOld = getTranslatedPost(old);
+                return (
+                  <Fragment key={index}>
+                    {translatedOld && (
+                      <PostPreview p="4" data={translatedOld} />
+                    )}
+                  </Fragment>
+                );
+              })}
+            </Box>
+          </>
+        )}
+      </Box>
     </Layout>
   );
 };
 
 export default IndexPage;
+
+export const getStaticProps = async () => {
+  const apollo = initializeApollo();
+  const { data } = await apollo.query<GetLatestPublicPostsQuery>({
+    query: GetLatestPublicPostsDocument,
+    variables: { skip: 0, take: 4 },
+  });
+
+  return {
+    props: { items: data.getLatestPublicPosts },
+    unstable_revalidate: 1,
+  };
+};
