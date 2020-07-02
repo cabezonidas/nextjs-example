3e501ea3244593d1c037234122b05b0866cbaafc
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-07-02 18:09:07 +1200

    Update config url

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,19 @@
-export const env = process?.env?.NODE_ENV;
-export const baseUrl =
-  env === "development"
-    ? "http://localhost:8899"
-    : "https://api.javascript.kiwi";
-export const graphqlUrl = `${baseUrl}/graphql`;
+export const env = process?.env?.NEXT_PUBLIC_ENV;
+const baseDomain = (() => {
+  if (env === "development") {
+    return { api: "http://localhost:8899", website: "http://localhost:3000" };
+  } else if (env === "development") {
+    return {
+      api: "https://api.javascript.kiwi",
+      website: "https://javascript.kiwi",
+    };
+  } else {
+    return {
+      api: "https://api.latamtradingclub.com",
+      website: "https://latamtradingclub.com",
+    };
+  }
+})();
+export const websiteUrl = baseDomain.website;
+export const graphqlUrl = `${baseDomain.api}/graphql`;
 export const companyName = "Latam Trading Club";
