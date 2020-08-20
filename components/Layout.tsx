1042b82d400a814a1f902b66e195c1c716c9e94e
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-08-20 19:43:49 +1200

    Add icons to identify wether the user is authenticated or not

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,8 +14,10 @@ import {
   Facebook,
   Messenger,
   Email,
+  SvgIcon,
+  Svg,
 } from "@cabezonidas/shop-ui";
-import { useGetPinnedPublicPathsQuery } from "../graphql-queries";
+import { useGetPinnedPublicPathsQuery, useMeQuery } from "../graphql-queries";
 import { usePostMapping } from "../utils/helpers";
 import { companyName } from "../utils/config";
 
@@ -29,6 +31,7 @@ const enUsRoutes = {
     home: "Home",
     about: "Us",
     pinned: "Investements",
+    profile: "Profile",
   },
   footer: "I'm here to stay!",
 };
@@ -37,6 +40,7 @@ const esArRoutes = {
     home: "Inicio",
     about: "Nosotros",
     pinned: "Inversiones",
+    perfil: "Perfil",
   },
   footer: "Estoy para quedarme!",
 };
@@ -63,6 +67,10 @@ const Layout = React.forwardRef<
     true
   );
   const { data } = useGetPinnedPublicPathsQuery();
+
+  const { data: meData } = useMeQuery();
+
+  const logged = !!meData?.me;
 
   return (
     <>
@@ -112,12 +120,24 @@ const Layout = React.forwardRef<
                 </Box>
               </Box>
             </Link>
-            <Box alignSelf="center">
+            <Box alignSelf="center" display="flex">
               <Link href="/pinned" passHref={true}>
                 <HeaderLink>{t("layout.routes.pinned")}</HeaderLink>
               </Link>
               <Link href="/about" passHref={true}>
                 <HeaderLink>{t("layout.routes.about")}</HeaderLink>
+              </Link>
+              <Link href="/profile" passHref={true}>
+                <HeaderLink
+                  display="flex"
+                  aria-label={t("layout.routes.profile")}
+                >
+                  {logged ? (
+                    <UserSvg alignSelf="center" width="16" height="16" />
+                  ) : (
+                    <KeySvg alignSelf="center" width="16" height="16" />
+                  )}
+                </HeaderLink>
               </Link>
             </Box>
           </Box>
@@ -181,3 +201,58 @@ const Layout = React.forwardRef<
 });
 
 export default Layout;
+
+export const KeySvg = React.forwardRef<SVGSVGElement, SvgIcon>((props, ref) => {
+  return (
+    <Svg viewBox="0 0 50 50" fill="currentColor" {...props} ref={ref}>
+      <path
+        d="M46.5,14.2c-0.3-0.4-0.7-0.7-1-1c-2.6-2.6-2.6-2.6-0.1-5.2c0.7-0.7,0.7-1.1,0-1.7c-1.1-1-2.2-2-3.1-3.1
+	c-0.8-1-1.3-0.9-2.2,0c-6.7,6.8-13.5,13.5-20.2,20.3c-0.9,0.9-1.6,1.2-3,0.8C9.2,21.9,1.4,28,1.6,35.9C1.8,43,8.1,48.5,15,47.5
+	c8.1-1.2,12.6-9.3,9.3-16.8c-0.3-0.8-0.5-1.2,0.2-1.9c2.5-2.4,5-4.9,7.4-7.4c0.6-0.6,0.9-0.5,1.4,0c1.4,1.5,2.9,3,4.4,4.4
+	c0.4,0.4,0.6,0.8,1.3,0.3c3-2.3,3.1-2.6,0.5-5.2c-0.4-0.4-0.8-0.8-1.2-1.2c-2.5-2.5-2.5-2.5,0.1-5c0.7-0.7,1.1-0.6,1.7,0
+	c1.4,1.5,2.8,2.9,4.3,4.3c0.3,0.3,0.6,1,1.2,0.3c0.9-1,2.2-1.8,2.7-2.9C48.7,15.8,47.1,15,46.5,14.2z M13.5,40.4
+	c-2.5,0-4.7-2.2-4.7-4.6c0-2.5,2.2-4.8,4.7-4.9c2.5,0,4.8,2.2,4.8,4.8C18.3,38.3,16.1,40.4,13.5,40.4z"
+      />
+    </Svg>
+  );
+});
+
+export const UserSvg = React.forwardRef<SVGSVGElement, SvgIcon>(
+  (props, ref) => {
+    return (
+      <Svg viewBox="0 0 32 32" {...props} ref={ref}>
+        <circle
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit={10}
+          cx="16"
+          cy="16"
+          r="14"
+        />
+        <circle
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit={10}
+          cx="16"
+          cy="13"
+          r="5"
+        />
+        <path
+          stroke="currentColor"
+          fill="none"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit={10}
+          d="M5.4,25.1c1.8-4.1,5.8-7,10.6-7c4.8,0,8.9,2.9,10.6,7"
+        />
+      </Svg>
+    );
+  }
+);
